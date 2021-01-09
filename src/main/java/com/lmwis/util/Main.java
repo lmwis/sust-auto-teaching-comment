f0303b36c0c727499b621f7c193a7ba7f52ad2fb
lmwis <detectiveygn@163.com>
2021-01-09 22:00:10 +0800

    feat<test>:date test

--- src/main/java/com/lmwis/util/Main.java
+++ src/main/java/com/lmwis/util/Main.java
@@ -1,5 +1,9 @@
 package com.lmwis.util;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * @Description:
  * @Author: lmwis
@@ -7,8 +11,23 @@ package com.lmwis.util;
  * @Version 1.0
  */
 public class Main {
-    public static void main(String[] args) {
-        AutoTeachingComment autoTeachingComment = new AutoTeachingComment();
-        autoTeachingComment.doComment("username","password");
+    public static void main(String[] args) throws ParseException {
+//        AutoTeachingComment autoTeachingComment = new AutoTeachingComment();
+//        autoTeachingComment.doComment("username","password");
+//        System.out.println(System.currentTimeMillis());
+//        System.out.println(new Date().getTime());
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .parse("1970-1-1 00:00:01").getTime());
+//        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+//                .format(0));
+        long time1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .parse("2020-1-1 00:00:00").getTime();
+        long time2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .parse("2021-1-1 00:00:00").getTime();
+        System.out.println(time2-time1);
+        System.out.println((time2-time1)/1000);
+        System.out.println((time2-time1)/(1000*60));
+        System.out.println((time2-time1)/(1000*60*24));
+        System.out.println((time2-time1)/(1000*60*60*24));
     }
 }
