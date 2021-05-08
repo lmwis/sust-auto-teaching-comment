6e9a4c0f3ee28557cac73ae30e5c1f91019b56fa
laimyuan <laimyuan@tencent.com>
2021-05-08 19:19:31 +0800

    feat:还能用

--- src/main/java/com/lmwis/util/Main.java
+++ src/main/java/com/lmwis/util/Main.java
@@ -12,22 +12,7 @@ import java.text.SimpleDateFormat;
  */
 public class Main {
     public static void main(String[] args) throws ParseException {
-//        AutoTeachingComment autoTeachingComment = new AutoTeachingComment();
-//        autoTeachingComment.doComment("username","password");
-//        System.out.println(System.currentTimeMillis());
-//        System.out.println(new Date().getTime());
-        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                .parse("1970-1-1 00:00:01").getTime());
-//        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-//                .format(0));
-        long time1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                .parse("2020-1-1 00:00:00").getTime();
-        long time2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                .parse("2021-1-1 00:00:00").getTime();
-        System.out.println(time2-time1);
-        System.out.println((time2-time1)/1000);
-        System.out.println((time2-time1)/(1000*60));
-        System.out.println((time2-time1)/(1000*60*24));
-        System.out.println((time2-time1)/(1000*60*60*24));
+        AutoTeachingComment autoTeachingComment = new AutoTeachingComment();
+        autoTeachingComment.doComment("学号","密码");
     }
 }
